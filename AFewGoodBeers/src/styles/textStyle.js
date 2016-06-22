58f186720403b0d98d9ed99ee68e666ce29e9737
Amy Fairbrother <afairbrother@sapient.com>
2016-06-22 16:49:27 -0400

    add initial three page navigation

---
+++ AFewGoodBeers/src/styles/textStyle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+var styles = StyleSheet.create({
+    header: {
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+});
+
+module.exports = styles;
